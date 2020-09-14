6856083edcbd30925c2f64548aa0817262efb50d
Papy82 <62964929+Papy82@users.noreply.github.com>
2020-09-13 23:59:52 -0400

    Add project files.

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductWebAPI.Models
+{
+    public class AppDbContext : DbContext
+    {
+        internal object products;
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        //DbSet is a representation of your Db
+        public DbSet<Product> product {get; set;}
+
+        internal class _db
+        {
+            internal static object products;
+
+            internal static void SaveChanges()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
